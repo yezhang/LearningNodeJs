86756a1bc74d85f8a0dc1ad4f2235912e0c54e28
yezhang <yezhang989@163.com>
2014-02-09 23:47:37 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require("http");
+var url = require("url");
+
+//var sys = require("sys");
+
+function start(route, handle){
+
+    function onRequest(request, response){
+        //var postData = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname +" received.");
+
+        route(handle, pathname, response, request);
+        /*request.setEncoding("utf8");
+
+        request.addListener("data", function(postDataChunk){
+            postData += postDataChunk;
+            console.log("Received POST Data chunk '" + postDataChunk + "'.");
+        });
+
+        request.addListener("end", function(){
+            route(handle, pathname, response, postData);
+        });*/
+
+
+
+    }
+
+    http.createServer(onRequest).listen(8888);
+
+    console.log("Server has started, listening on 8888");
+}
+
+exports.startServer = start;
